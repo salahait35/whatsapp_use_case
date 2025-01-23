835d0f8b980a0fa898fdc9a6b950c7f61071d468
salahait35 <136812449+salahait35@users.noreply.github.com>
2025-01-23 14:07:28 +0100

    okey going for new way to do

---
+++ backend-whatsapp-use-case/backend-whatsapp-use-case/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace backend_whatsapp_use_case
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
