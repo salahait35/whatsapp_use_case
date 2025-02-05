ce2ef26259e9111153dfb92907895b4f3d80fb95
salahait35 <136812449+salahait35@users.noreply.github.com>
2025-02-05 17:12:41 +0100

    cleaned repo

--- WebappWhatsapp/WebappWhatsapp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebappWhatsapp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
